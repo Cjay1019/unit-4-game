3d38fbd28be4ef3ac7431621e76b50aaaeeb52ce
Cjay1019 <cjay741@gmail.com>
2018-11-10 14:08:06 -0500

    changed characters to objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,36 @@
 
 
 // CHARACTER OBJECTS
-var char1 = {
-    name: "char1",
-    hp: 100,
-    attack: 6,
-    counter: 5,
-    status: "choosing"
+var characters = {
+    char1: {
+        name: "char1",
+        hp: 100,
+        attack: 6,
+        counter: 5,
+        status: "choosing"
+    },
+    char2: {
+        name: "char2",
+        hp: 120,
+        attack: 8,
+        counter: 10,
+        status: "choosing"
+    },
+    char3: {
+        name: "char3",
+        hp: 150,
+        attack: 10,
+        counter: 20,
+        status: "choosing"
+    },
+    char4: {
+        name: "char4",
+        hp: 180,
+        attack: 12,
+        counter: 25,
+        status: "choosing"
+    }
 }
-var char2 = {
-    name: "char2",
-    hp: 120,
-    attack: 8,
-    counter: 10,
-    status: "choosing"
-}
-var char3 = {
-    name: "char3",
-    hp: 150,
-    attack: 10,
-    counter: 20,
-    status: "choosing"
-}
-var char4 = {
-    name: "char4",
-    hp: 180,
-    attack: 12,
-    counter: 25,
-    status: "choosing"
-}
-
 // Initializing
 $("#alert-1").text("Choose Your Character");
 
@@ -37,7 +38,7 @@ $("#alert-1").text("Choose Your Character");
 $(".choose-char").on("click", function (e) {
     var classString = e.currentTarget.getAttribute("class");
     var idString = e.currentTarget.getAttribute("id");
-
+    characters[idString].status = "not choosing"
     // Choose your character and move others to enemy section
     if (classString.includes("choose-char")) {
         $("#" + idString).hide().appendTo("#your-box").show("fast");
@@ -46,7 +47,15 @@ $(".choose-char").on("click", function (e) {
         $("#choose-col").attr("id", "enemies-col");
         $(".choose-char").attr("class", "enemy-char ");
         $("#alert-1").text("Choose An Enemy to Fight");
-        window[idString].status = "your";
+        characters[idString].status = "your";
+        for (var key in characters) {
+            if (characters[key].status === "choosing") {
+                characters[key].status = "enemy";
+            }
+        }
+        console.log(characters.char1.status);
+        console.log(characters.char4.status);
+        
     }
 
     // Choose your defender and lock other enemies
